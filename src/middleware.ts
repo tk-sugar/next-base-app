a2eea9e2e23f30d778cc72fb4fa3b304ed491b38
tk-sugar <19794421+tk-sugar@users.noreply.github.com>
2026-03-17 16:11:33 +0900

    Fix vercel error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/auth.config";
 
-export const { auth: middleware } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+export default auth;
 
 export const config = {
   matcher: ["/repos/:path*"],
